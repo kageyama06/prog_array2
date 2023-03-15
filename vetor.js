fda251be4768f97f48b68008d592232b0406d4f6
Thiago <thiago_r_araujo@estudante.sesisenai.or.br>
2023-03-15 10:27:00 -0300

    Novos botões adicionados

--- vetor.js
+++ vetor.js
@@ -5,11 +5,26 @@ function inserir(){
     let cidade = document.getElementById('cidade').value
     valor_cidade.push(cidade)
     console.log(valor_cidade)
-    res.innerHTML = cidade
+    res.innerHTML = valor_cidade
+}
+
+function insert_inicio(){
+    let cidade = document.getElementById('cidade').value
+    valor_cidade.unshift(cidade)
+    console.log(valor_cidade)
+    res.innerHTML = valor_cidade
 }
 
 function excluir(){
+    let cidade = document.getElementById('cidade').value
     valor_cidade.pop(cidade)
     console.log(valor_cidade)
-    res.innerHTML = cidade
+    res.innerHTML = valor_cidade
+}
+
+function excluir_final(){
+    let cidade = document.getElementById('cidade').value
+    valor_cidade.shift(cidade)
+    console.log(valor_cidade)
+    res.innerHTML = valor_cidade
 }
