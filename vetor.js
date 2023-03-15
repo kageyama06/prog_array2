aa0bc83c6490d22cac1155b928934494aeef29e4
Thiago <thiago_r_araujo@estudante.sesisenai.or.br>
2023-03-15 09:40:11 -0300

    incluindo innerHTML para imprimir na telinha

--- vetor.js
+++ vetor.js
@@ -1,12 +1,15 @@
 let valor_cidade = []  //variável global do tipo array
+let res = document.getElementById('res')
 
 function inserir(){
     let cidade = document.getElementById('cidade').value
     valor_cidade.push(cidade)
     console.log(valor_cidade)
+    res.innerHTML = cidade
 }
 
 function excluir(){
     valor_cidade.pop(cidade)
     console.log(valor_cidade)
+    res.innerHTML = cidade
 }
