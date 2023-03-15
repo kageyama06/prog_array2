71a49bec9216ee0ea882933db3685c326989da92
Thiago <thiago_r_araujo@estudante.sesisenai.or.br>
2023-03-15 09:00:16 -0300

    programando push e pop no array

--- vetor.js
+++ vetor.js
@@ -1,5 +1,12 @@
-let cidade = ['Tijucas','Itapema',1,2,'@','#']  //variável global do tipo array
+let valor_cidade = []  //variável global do tipo array
 
 function inserir(){
-    console.log(cidade)
+    let cidade = document.getElementById('cidade').value
+    valor_cidade.push(cidade)
+    console.log(valor_cidade)
+}
+
+function excluir(){
+    valor_cidade.pop(cidade)
+    console.log(valor_cidade)
 }
